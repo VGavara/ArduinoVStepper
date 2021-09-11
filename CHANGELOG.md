c121c34b619b880d5203c3cbcd7a95b0ce9d2d54
Vicente Gavara <vicente.gavara@nbn23.com>
2021-09-11 19:05:37 +0200

    Directory structure created

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Arduino Virtual Stepper library changelog
+
+- [Version 0](#version-0)
+  * [Release 0.0.1](##release-v0.0.1)
+
+# Version 0
+
+## Release v0.0.1
+- Initial release containing... (_name the classes and main features included in this releas_)
---
+++ src/vstepper.h
@@ -0,0 +1,11 @@
+// vstepper.h
+// Library implementing stepper motor controllers
+// Copyright (c) Vicente Gavara. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef VSTEPPER_H
+#define VSTEPPER_H
+
+// Include here the project libraries
+
+#endif
